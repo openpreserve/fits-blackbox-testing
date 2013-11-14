67646831ae2aa89345f3d24dfe4bd6e87edc1f4b
Petar Petrov <me@petarpetrov.org>
2013-11-14 09:58:54 +0100

    adding fbt cl tool stub

---
+++ fbt/src/main/java/edu/harvard/hul/fbt/App.java
@@ -0,0 +1,13 @@
+package edu.harvard.hul.fbt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
