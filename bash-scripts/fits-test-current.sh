382b8512dd0138dd3be9b40842dfc74eccf60308
carlwilson <carl@openplanetsfoundation.org>
2013-11-28 18:33:55 +0000

    Added script tobe called by git-bisect.

---
+++ bash-scripts/fits-test-current.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+##
+# Bash script to build and test the current FITS checkout from source.
+#
+# author Carl Wilson carl@openplanetsfoundation.org
+#
+# Script expects 2 parameters:
+#
+#  $1 path to the FITS testing tool to use
+#     Mandatory
+#
+#  $2 path to root directory of test corpora to use 
+#     Mandatory
+#
+#  $3 path to root directory for output
+#	  Mandatory
+##
